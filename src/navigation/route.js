dc4e0d7f69ad286b7eec29bf393766faa3b8ce12
Kraisorn.R <kraisorn204@gmail.com>
2020-06-08 10:01:46 +0700

    Create route.js

---
+++ src/navigation/route.js
@@ -0,0 +1,18 @@
+const Route = {
+    App: {
+        HomeTab: {
+            Home: {
+                RouteName: "Home",
+                Component: "",
+            },
+            Details: {
+                RouteName: "Details",
+                Component: "",
+            }
+        }
+    },
+    SignIn: {
+        RouteName: "SignIn",
+        Component: "",
+    }
+}
